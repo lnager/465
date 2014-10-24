ce12c4e032eded5cb9b8c885f5a99ec76b00d2e1
lnager <lnager@mail.csuchico.edu>
2014-10-24 06:35:14 +0000

    both lookups work, but cannot handle wrong key/name, and searching w regex

--- doi_server/app/controllers/home_controller.rb
+++ doi_server/app/controllers/home_controller.rb
@@ -8,12 +8,13 @@ def show
  @query = params[:query]
  @name_query = params[:name_query]
  
- if @query
+ if @query && @query != ""
    @doi_result = Doi.find_by(key: @query)
  end
 
+
  if @name_query && @name_query != ""
-@doi_result = Doi.all.map { |doi| doi if /#{@name_query}/ =~ doi.name }.compact
+   @doi_regex = Doi.find_by(name: @name_query)
  end
 
 
